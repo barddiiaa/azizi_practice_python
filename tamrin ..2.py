f8baf06e435bf95ddd58daa83bb1e88a939895d1
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-07 13:08:04 +0330

    tanrin 2.py

---
+++ tamrin ..2.py
@@ -0,0 +1,11 @@
+# gereftan ghaede
+ghaede = int(input("ghaede ra vared konid"))
+
+# gereftan ertefa
+ertefa = int(input("ertefaa"))
+
+# mohasebe masahat
+masahat = (ghaede * ertefa) / 2
+
+# namayesh khrorgi
+print("masaht: ",masahat)
