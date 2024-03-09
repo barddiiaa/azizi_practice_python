46033e5705afce4100c905189f880d9a94e1c0a0
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-09 02:45:55 -0800

    Update tamrin ..2.py

--- tamrin ..2.py
+++ tamrin ..2.py
@@ -1,11 +1,7 @@
-# gereftan ghaede
-ghaede = int(input("ghaede ra vared konid"))
+r = int(input("shoa ra vared knid : "))
+h= int(input("ertefa ra vared knid : "))
+v = 3.14 *r*r*h
+s =2 * 3.14 *r*h + 2*3.14* r*r
+print(f"hajm = {v}")
+print(f"masahat kol  = {s}")
 
-# gereftan ertefa
-ertefa = int(input("ertefaa"))
-
-# mohasebe masahat
-masahat = (ghaede * ertefa) / 2
-
-# namayesh khrorgi
-print("masaht: ",masahat)
