20f3f83a029c7f9bb2df63766345bef8ad05aaea
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-10 23:25:14 +0330

    tamrin3.py

---
+++ tamrin ..3.py
@@ -0,0 +1,5 @@
+v = float(input("sorat bad ra vared knid:"))
+T = float(input("dama ra wared konid:"))
+wind_child = 13.13 + (0.62 * T) - (13.95 * v) + (0.486 * T * v)
+print(f"vazesh bad:{wind_child}")
+
