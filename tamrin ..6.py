28a94bd56c2a1b9684e35836647b46b0e1d234a4
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-13 17:24:52 +0330

    Add files via upload

---
+++ tamrin ..6.py
@@ -0,0 +1,6 @@
+R1 = float(input(("meghdar R1 ra vared knid:"))
+R2 = float(input(("meghdar R2 ra vared knid:"))
+R3 = flout(input(("meghdar R3 ra vered knid:"))
+Moghavemat_kol = (1/R1 + 1/R2 + 1/R3)  
+print(f"Moghavemat kol:{Moghavemat_kol}")
+
