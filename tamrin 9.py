f5b4dc5aa397bc1afc30ef043cee4098bc5d4989
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-21 13:43:50 +0330

    tamrin.9

---
+++ tamrin 9.py
@@ -0,0 +1,5 @@
+sorat_avalie = float(input("sorat avalie vared knid:"))
+sorat_nahaie = float(input("sorat nahaie vared knid:"))
+zaman = float(input("zaman vared konid:"))
+shetab = (sorat_avalie-sorat_nahaie)/zaman
+print("shetab ranande")
