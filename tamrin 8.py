7e90352711373e190e1f18421c6d9fffb1ba357e
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-21 12:22:45 +0330

    tamrin.8

---
+++ tamrin 8.py
@@ -0,0 +1,8 @@
+hoghogh = float(input("hoghogh ra vared konid :"))
+padash_hoghogh = 15
+padash = (padash_hoghogh / 100) * hoghogh
+kol_hoghogh = hoghogh + padash
+print("hoghogh karmand:" , hoghogh)
+print("padash hoghogh:"  , padash)
+print(f"padash : {hoghogh}")
+
