3302d5f637f82d4b801d49dd4b6da93ac2456dc7
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-05-10 04:19:48 -0700

    Add files via upload

---
+++ best tom.py
@@ -0,0 +1,28 @@
+import re
+
+def interpret_statement(statement):
+    if 'not' in statement:
+        return False
+    return True
+
+def evaluate_suspect(suspect, statements):
+    true_count = 0
+    false_count = 0
+    for statement in statements:
+        if interpret_statement(statement[suspect]):
+            true_count += 1
+        else:
+            false_count += 1
+    return true_count == false_count
+
+def guilty(sus):
+    for suspect in sus:
+        if evaluate_suspect(suspect, sus.values()):
+            print(f'{suspect} is the thief')
+
+sus = {'a': [False, 'b', True],
+       'b': ['not d', 'not a', 'not b'],
+       'c': [True, 'not b', True],
+       'd': ['d', False, 'a']}
+
+guilty(sus)
