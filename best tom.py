f336b4e2edee4d9ee38766725605adac1d026722
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-06-13 20:39:30 +0330

    Update est tom.py
    D

--- best tom.py
+++ best tom.py
@@ -1,28 +1,56 @@
-import re
+import sqlite3
 
-def interpret_statement(statement):
-    if 'not' in statement:
-        return False
-    return True
+def create_table(connection):
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY, date TEXT, amount REAL)''')
+    connection.commit()
 
-def evaluate_suspect(suspect, statements):
-    true_count = 0
-    false_count = 0
-    for statement in statements:
-        if interpret_statement(statement[suspect]):
-            true_count += 1
+def add_cost(connection, date, amount):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO expenses (date, amount) VALUES (?, ?)", (date, amount))
+    connection.commit()
+
+def show_costs(connection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM expenses")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+def delete_cost(connection, expense_id):
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM expenses WHERE id=?", (expense_id,))
+    connection.commit()
+
+def main():
+    cnt = sqlite3.connect('expense.db')
+    create_table(cnt)
+
+    while True:
+        print("1. Add cost")
+        print("2. Show costs")
+        print("3. Delete cost")
+        print("4. Exit")
+
+        option = input("Choose an option: ")
+
+        if option == '1':
+            date = input("Enter the date of deposit: ")
+            amount = float(input("Enter amount of money: "))
+            add_cost(cnt, date, amount)
+            print("Successfully added")
+        elif option == '2':
+            show_costs(cnt)
+        elif option == '3':
+            expense_id = int(input("Enter ID of deposit: "))
+            delete_cost(cnt, expense_id)
+            print("Successfully deleted")
+        elif option == '4':
+            break
         else:
-            false_count += 1
-    return true_count == false_count
+            print("Wrong option, try again")
 
-def guilty(sus):
-    for suspect in sus:
-        if evaluate_suspect(suspect, sus.values()):
-            print(f'{suspect} is the thief')
+    cnt.close()
 
-sus = {'a': [False, 'b', True],
-       'b': ['not d', 'not a', 'not b'],
-       'c': [True, 'not b', True],
-       'd': ['d', False, 'a']}
-
-guilty(sus)
+if name == "main":
+    main()
