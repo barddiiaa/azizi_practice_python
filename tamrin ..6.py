670dccd2118903526d35700172d1652a99ee4772
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-06-23 11:51:02 +0330

    Antegral ..6.py

--- tamrin ..6.py
+++ tamrin ..6.py
@@ -1,6 +1,12 @@
-R1 = float(input(("meghdar R1 ra vared knid:"))
-R2 = float(input(("meghdar R2 ra vared knid:"))
-R3 = flout(input(("meghdar R3 ra vered knid:"))
-Moghavemat_kol = (1/R1 + 1/R2 + 1/R3)    
-print(f"Moghavemat kol:{Moghavemat_kol}")
+import numpy as np
+from scipy import integrate
 
+# تعریف تابعی که می‌خواهیم انتگرال آن را محاسبه کنیم
+def integrand(x, y):
+    return x**2 + y**2
+
+# محاسبه انتگرال دوگانه
+result, error = integrate.dblquad(integrand, 0, 1, lambda x: x**2, lambda x: x)
+
+print(f'Result of the double integral: {result}')
+print(f'Estimated error: {error}')
